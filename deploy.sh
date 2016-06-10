c3ca0dd4b71c0268a4bff3bd38f4d8b85b9b4501
Jordan <jordanjm@users.noreply.github.com>
2016-06-10 14:33:30 -0600

    Found a working rsync command

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ KEYLOCATION="$HOME/PATH-TO-KEY"
 LOCALWEBSITEFILES="$HOME/JEKYLL-SITE-DIRECTORY/_site/"
 REMOTEUSERNAME="REMOTE-USERNAME"
 REMOTESERVER="REMOTE-SERVER"
+REMOTEWEBPATH="REMOTE-WEBROOT-DIRECTORY"
+PORT="PORT-NUMBER"
 
 #Check if the _site directory exists and delete it
 if [ -d "$LOCALWEBSITEFILES" ]; then
@@ -13,5 +15,11 @@ fi
 #Build the Jekyll Site
 jekyll build
 
+#Generate new .htaccess file
+echo "RewriteEngine On" >> $LOCALWEBSITEFILES/.htaccess
+echo "RewriteCond %{SERVER_PORT} 80" >> $LOCALWEBSITEFILES/.htaccess
+echo "RewriteRule ^(.*)$ https://www.$REMOTEUSERNAME.com/$1 [R=301,L]" >> $LOCALWEBSITEFILES/.htaccess
+echo "ErrorDocument 404 https://www.$REMOTEUSERNAME.com/404/index.html" >> $LOCALWEBSITEFILES/.htaccess
+
 #Copy the Jekyll Site To the Server
-rsync --verbose --compress --hard-links --checksum --recursive --rsh="ssh -p 4242 -i $KEYLOCATION" $LOCALWEBSITEFILES $REMOTEUSERNAME@$REMOTESERVER:
+rsync -avz --checksum -e "ssh -p $PORT -i $KEYLOCATION" $LOCALWEBSITEFILES $REMOTEUSERNAME@$REMOTESERVER:$REMOTEWEBPATH
